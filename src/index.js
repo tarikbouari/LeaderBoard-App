3a60864e3f898f7123ef9bbeacd13443a0b1ba79
Rikstone <tarikbouari@gmail.com>
2022-08-23 07:08:34 -0700

    add class on input

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import Scores from './functionaliies.js';
 
 Scores;
 const refresh = document.getElementById('refresh');
-const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+const container = document.getElementById('name-board');
+
+const renderScore = () => {
+    const data = JSON.parse(localStorage.getItem('data')) || [];
+
+    data.forEach( (item,index) => {
+        let load = `<div class="flex"> 
+                        <h3> ${item.playerName}<h3>
+                        <h3>${item.score}<h3>
+                    <div>`
+                    item.index++;
+
+           container.innerHTML+= load;        
+    })
+
+}
+
+submit.addEventListener('click',(e) =>)
 
