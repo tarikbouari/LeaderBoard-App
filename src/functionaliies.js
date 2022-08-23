70a978e49a2d79810df9583cb8d7ff5d9b8067a2
Rikstone <tarikbouari@gmail.com>
2022-08-23 09:44:37 -0700

    Refactor code

--- src/functionaliies.js
+++ src/functionaliies.js
@@ -4,12 +4,15 @@ export default class Scores {
     this.score = score;
     this.index = index;
   }
+
   addScore() {
+    const data = JSON.parse(localStorage.getItem('data')) || [];
     const scoreList = {
       playerName: this.playerName,
       score: this.score,
       index: this.index,
     };
     data.push(scoreList);
+    localStorage.setItem('data', JSON.stringify(data));
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 import './style.css';
-import Scores from './functionaliies.js'; 
+import Scores from './functionaliies.js';
 
-
-Scores;
 const refresh = document.getElementById('refresh');
 const submit = document.getElementById('submit');
 const container = document.getElementById('name-board');
+const nameInput = document.getElementById('name');
+const form = document.getElementById('form');
+const scoreInput = document.getElementById('score');
 
 const renderScore = () => {
-    const data = JSON.parse(localStorage.getItem('data')) || [];
+  const data = JSON.parse(localStorage.getItem('data')) || [];
 
-    data.forEach( (item,index) => {
-        let load = `<div class="flex"> 
-                        <h3> ${item.playerName}<h3>
-                        <h3>${item.score}<h3>
-                    <div>`
-                    item.index++;
+  data.forEach((item) => {
+    const load = `<div class="flex"> 
+                        <h3> ${item.playerName}:<h3>
+                        <h3 class="child">${item.score}<h3>
+                    <div>`;
 
-           container.innerHTML+= load;        
-    })
+    container.innerHTML += load;
+  });
+};
+renderScore();
 
-}
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  const nameInpute = nameInput.value;
+  const scoreInpute = scoreInput.value;
 
-submit.addEventListener('click',(e) =>)
+  if ((nameInpute && scoreInpute) === '') return 'value missing';
 
+  const newData = new Scores(nameInpute, scoreInpute);
+  newData.addScore();
+  container.innerHTML = '';
+  renderScore();
+  form.reset();
+  return e;
+});
+
+refresh.addEventListener('click', () => {
+  document.location.reload();
+});
