f5e3c2d4f30d537c75118013ab09159a4bc8c920
Rikstone <tarikbouari@gmail.com>
2022-08-24 14:15:21 -0700

    declare variable

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./style.css";
 import { url } from "./game.js";
+
+const refresh = document.getElementById("refresh");
+const submit = document.getElementById("submit");
+const container = document.getElementById("name-board");
