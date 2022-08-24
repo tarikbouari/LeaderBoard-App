eeac18390464663dcebabff49a7ea56ba2a8d6a3
Rikstone <tarikbouari@gmail.com>
2022-08-24 16:44:30 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import "./style.css";
-import { url } from "./game.js";
+import './style.css';
+import { sendData, getScore } from './game.js';
 
-const refresh = document.getElementById("refresh");
-const submit = document.getElementById("submit");
-const container = document.getElementById("name-board");
-const nameInput = document.getElementById("name");
-const form = document.getElementById("form");
-const scoreInput = document.getElementById("score");
+const refresh = document.getElementById('refresh');
+const submit = document.getElementById('submit');
+const container = document.getElementById('name-board');
+const nameInput = document.getElementById('name');
+const form = document.getElementById('form');
+const scoreInput = document.getElementById('score');
 
-submit.addEventListener("click", (e) => {
+submit.addEventListener('click', (e) => {
   e.preventDefault();
-  if ((nameInput.value && scoreInput.value) === "") return "value missing";
+  if ((nameInput.value && scoreInput.value) === '') return 'value missing';
   sendData({
     user: nameInput.value,
     score: scoreInput.value,
@@ -19,37 +19,9 @@ submit.addEventListener("click", (e) => {
   return e;
 });
 
-const sendData = async (array) => {
-  const res = await fetch(url, {
-    method: "Post",
-    body: JSON.stringify(array),
-
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
-  const storage = await res.json();
-  console.log(storage);
-};
-
-const getScore = async () => {
-  const res = await fetch(url);
-  const storage = await res.json();
-  const dataScores = storage.result;
-  dataScores.forEach((item) => {
-    const load = `<div class="flex"> 
-                        <li> ${item.user}: ${item.score}</li>
-                    <div>`;
-
-    container.innerHTML += load;
-  });
-};
-
 getScore();
 
-refresh.addEventListener("click", () => {
-  container.innerHTML ='';
+refresh.addEventListener('click', () => {
+  container.innerHTML = '';
   getScore();
 });
