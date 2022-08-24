16e29a33ccb849dbc82a7f18b555395400b3f1b0
Rikstone <tarikbouari@gmail.com>
2022-08-24 16:08:49 -0700

    update games.js

--- src/game.js
+++ src/game.js
@@ -1,24 +1,5 @@
 
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N8jE12XJo0QEuAY66uTz/scores/';
 
-const fetchUrl = async () => {
-
-    const res = await fetch(
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
-      {
-        method: "POST",
-        body: JSON.stringify({
-            name: "Fifa Saison",
-          }),
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-          },
-        });
-        const storage = await res.json();
-        console.log(storage);
-      };
-      
-      export { fetchUrl, url };
+export { url };
       
