f9fa2ef4558bc5e0b9fcfdb7909b78ca38935920
Rikstone <tarikbouari@gmail.com>
2022-08-24 13:56:08 -0700

    create async function

--- src/game.js
+++ src/game.js
@@ -1,2 +1,9 @@
 
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N8jE12XJo0QEuAY66uTz/scores/';
+
+const fetchUrl = async () => {
+
+    const res = await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+      {
+        method: "POST",
