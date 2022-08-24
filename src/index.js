20dd904848e585855d965a6713fe1cdff443951c
Rikstone <tarikbouari@gmail.com>
2022-08-24 14:16:01 -0700

    update  variable declaration

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import { url } from "./game.js";
 const refresh = document.getElementById("refresh");
 const submit = document.getElementById("submit");
 const container = document.getElementById("name-board");
+const nameInput = document.getElementById("name");
+const form = document.getElementById("form");
+const scoreInput = document.getElementById("score");
