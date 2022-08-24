1dd2d2f5dc0429fad25a21ff48457c81c5c3da1b
Rikstone <tarikbouari@gmail.com>
2022-08-24 14:17:20 -0700

    create function render

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ const container = document.getElementById("name-board");
 const nameInput = document.getElementById("name");
 const form = document.getElementById("form");
 const scoreInput = document.getElementById("score");
+
+const renderScore = () => {
+  const data = JSON.parse(localStorage.getItem("data")) || [];
+
+  data.forEach((item) => {
+    const load = `<div class="flex"> 
+                        <h3> ${item.playerName}:<h3>
+                        <h3 class="child">${item.score}<h3>
+                    <div>`;
+
+    container.innerHTML += load;
+  });
+};
