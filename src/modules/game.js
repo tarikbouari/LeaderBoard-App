868bd11f8a79f9db94c62c170dc70ef8b6a4a5b5
Rikstone <tarikbouari@gmail.com>
2022-08-25 07:22:44 -0700

    update module.js

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,14 +1,15 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N8jE12XJo0QEuAY66uTz/scores/';
-const container = document.getElementById('name-board');
+const url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N8jE12XJo0QEuAY66uTz/scores/";
+const container = document.getElementById("name-board");
 const sendData = async (array) => {
   const res = await fetch(url, {
-    method: 'Post',
+    method: "Post",
     body: JSON.stringify(array),
 
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
   });
   const storage = await res.json();
@@ -20,8 +21,10 @@ const getScore = async () => {
   const storage = await res.json();
   const dataScores = storage.result;
   dataScores.forEach((item) => {
-    const load = `<div class="flex"> 
-                          <li> ${item.user}: ${item.score}</li>
+    const load = `<div class="flex">
+                          
+                          <li> ${item.user}: </li>
+                          <li> ${item.score}: </li>    
                       <div>`;
 
     container.innerHTML += load;
